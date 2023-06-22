5db1cb9517761bd5380f1f9bd2b27f3a5001f1f5
Ishneet Singh <ishneet100@gmail.com>
2023-06-22 14:14:56 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,78 +1,168 @@
-import React, { useEffect, useReducer, useState, useContext } from "react";
+import React, { useEffect, useReducer, useState, useContext, useCallback } from "react";
 import axios from "axios";
 import { CartContext } from "../context/Store";
 import ProductCard from "../components/ProductCard";
+import { debounce } from "lodash";
 
 const initialState = {
   products: [],
   filteredProducts: [],
   search: "",
   selectedCategory: "All",
+  isLoading: true,
+  error: null,
+  minPrice: "",
+  maxPrice: "",
+  sortOption: "default",
+  page: 1,
+  pageSize: 12,
+  totalPages: 1,
 };
 
 const reducer = (state, action) => {
   switch (action.type) {
     case "SET_PRODUCTS":
-      return { ...state, products: action.payload };
+      return { ...state, products: action.payload, isLoading: false };
     case "SET_FILTERED_PRODUCTS":
       return { ...state, filteredProducts: action.payload };
     case "SET_SEARCH":
       return { ...state, search: action.payload };
     case "SET_SELECTED_CATEGORY":
       return { ...state, selectedCategory: action.payload };
+    case "SET_ERROR":
+      return { ...state, error: action.payload, isLoading: false };
+    case "SET_PRICE_RANGE":
+      return {
+        ...state,
+        minPrice: action.payload.minPrice,
+        maxPrice: action.payload.maxPrice,
+      };
+    case "SET_SORT_OPTION":
+      return { ...state, sortOption: action.payload };
+    case "SET_PAGE":
+      return { ...state, page: action.payload };
+    case "SET_TOTAL_PAGES":
+      return { ...state, totalPages: action.payload };
     default:
       return state;
   }
 };
 
+const Category = ({ category, selectedCategory, handleCategoryChange }) => (
+  <li
+    className={selectedCategory === category ? "active" : ""}
+    onClick={() => handleCategoryChange(category)}
+  >
+    {category}
+  </li>
+);
+
 const Home = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const { cart, addToCart, removeFromCart, clearCart } = useContext(CartContext);
+  const [debouncedSearch, setDebouncedSearch] = useState("");
 
   const addItemToCart = (item) => {
     addToCart(item);
   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("https://fakestoreapi.com/products");
-        dispatch({ type: "SET_PRODUCTS", payload: response.data });
-      } catch (error) {
-        console.log("Error fetching products:", error);
-      }
-    };
+  const handleCategoryChange = useCallback(
+    (category) => {
+      dispatch({ type: "SET_SELECTED_CATEGORY", payload: category });
+    },
+    []
+  );
 
-    fetchData();
+  const handlePriceRangeChange = (event) => {
+    const { name, value } = event.target;
+    dispatch({ type: "SET_PRICE_RANGE", payload: { ...state, [name]: value } });
+  };
+
+  const handleSortOptionChange = (event) => {
+    const { value } = event.target;
+    dispatch({ type: "SET_SORT_OPTION", payload: value });
+  };
+
+  const handlePageChange = (page) => {
+    dispatch({ type: "SET_PAGE", payload: page });
+  };
+
+  const fetchProducts = useCallback(async () => {
+    try {
+      const response = await axios.get("https://fakestoreapi.com/products");
+      dispatch({ type: "SET_PRODUCTS", payload: response.data });
+    } catch (error) {
+      dispatch({ type: "SET_ERROR", payload: "Error fetching products" });
+    }
   }, []);
-
+  
   useEffect(() => {
-    const filterProducts = () => {
-      let filtered = state.products;
+    fetchProducts();
+  }, [fetchProducts]);
 
+  const applyFilters = useCallback(
+    debounce(() => {
+      let filtered = state.products;
+  
       if (state.selectedCategory !== "All") {
         filtered = filtered.filter(
           (product) => product.category === state.selectedCategory
         );
       }
-
+  
       if (state.search) {
         filtered = filtered.filter((product) =>
           product.title.toLowerCase().includes(state.search.toLowerCase())
         );
       }
-
+  
+      if (state.minPrice) {
+        filtered = filtered.filter(
+          (product) => parseFloat(product.price) >= parseFloat(state.minPrice)
+        );
+      }
+  
+      if (state.maxPrice) {
+        filtered = filtered.filter(
+          (product) => parseFloat(product.price) <= parseFloat(state.maxPrice)
+        );
+      }
+  
+      // Sort products based on the selected sort option
+      switch (state.sortOption) {
+        case "price-low-to-high":
+          filtered = filtered.sort(
+            (a, b) => parseFloat(a.price) - parseFloat(b.price)
+          );
+          break;
+        case "price-high-to-low":
+          filtered = filtered.sort(
+            (a, b) => parseFloat(b.price) - parseFloat(a.price)
+          );
+          break;
+        default:
+          // No sorting or default sorting
+          break;
+      }
+  
       dispatch({ type: "SET_FILTERED_PRODUCTS", payload: filtered });
-    };
+    }, 300),
+    [
+      state.products,
+      state.selectedCategory,
+      state.search,
+      state.minPrice,
+      state.maxPrice,
+      state.sortOption,
+    ]
+  );
+  
 
-    filterProducts();
-  }, [state.search, state.selectedCategory, state.products]);
+  
 
-  const handleCategoryChange = (category) => {
-    dispatch({ type: "SET_SELECTED_CATEGORY", payload: category });
-  };
-
-  const categories = [...new Set(state.products.map((product) => product.category))];
+  useEffect(() => {
+    applyFilters();
+  }, [applyFilters]);
 
   return (
     <>
@@ -82,49 +172,80 @@ const Home = () => {
           type="text"
           placeholder="Search products..."
           value={state.search}
-          onChange={(e) =>
-            dispatch({ type: "SET_SEARCH", payload: e.target.value })
-          }
+          onChange={(e) => setDebouncedSearch(e.target.value)}
         />
       </div>
       <div className="mx-auto grid w-full max-w-7xl items-center space-y-4 px-2 py-10 md:grid-cols-2 md:gap-6 md:space-y-0 lg:grid-cols-4">
         <div>
           <h2>Categories:</h2>
           <ul>
-            <li
-              className={state.selectedCategory === "All" ? "active" : ""}
-              onClick={() => handleCategoryChange("All")}
-            >
-              All
-            </li>
-            {categories.map((category) => (
-              <li
-                key={category}
-                className={
-                  state.selectedCategory === category ? "active" : ""
-                }
-                onClick={() => handleCategoryChange(category)}
-              >
-                {category}
-              </li>
-            ))}
+            <Category
+              category="All"
+              selectedCategory={state.selectedCategory}
+              handleCategoryChange={handleCategoryChange}
+            />
+            <Category
+              category="men's clothing"
+              selectedCategory={state.selectedCategory}
+              handleCategoryChange={handleCategoryChange}
+            />
+            <Category
+              category="jewelery"
+              selectedCategory={state.selectedCategory}
+              handleCategoryChange={handleCategoryChange}
+            />
+            <Category
+              category="electronics"
+              selectedCategory={state.selectedCategory}
+              handleCategoryChange={handleCategoryChange}
+            />
+            <Category
+              category="women's clothing"
+              selectedCategory={state.selectedCategory}
+              handleCategoryChange={handleCategoryChange}
+            />
           </ul>
+          <div>
+            <h2>Price Range:</h2>
+            <input
+              type="number"
+              name="minPrice"
+              placeholder="Min Price"
+              value={state.minPrice}
+              onChange={handlePriceRangeChange}
+            />
+            <input
+              type="number"
+              name="maxPrice"
+              placeholder="Max Price"
+              value={state.maxPrice}
+              onChange={handlePriceRangeChange}
+            />
+          </div>
+          <div>
+            <h2>Sort By:</h2>
+            <select value={state.sortOption} onChange={handleSortOptionChange}>
+              <option value="default">Default</option>
+              <option value="price-low-to-high">Price: Low to High</option>
+              <option value="price-high-to-low">Price: High to Low</option>
+            </select>
+          </div>
         </div>
-        {state.search === ""
-          ? state.filteredProducts.map((item) => (
-              <ProductCard
-                key={item.id}
-                i={item}
-                addItemToCart={() => addItemToCart(item)}
-              />
-            ))
-          : state.filteredProducts.map((item) => (
-              <ProductCard
-                key={item.id}
-                i={item}
-                addItemToCart={() => addItemToCart(item)}
-              />
-            ))}
+        {state.isLoading ? (
+          <p>Loading...</p>
+        ) : state.error ? (
+          <p>Error: {state.error}</p>
+        ) : state.filteredProducts.length === 0 ? (
+          <p>No products found.</p>
+        ) : (
+          state.filteredProducts.map((item) => (
+            <ProductCard
+              key={item.id}
+              i={item}
+              addItemToCart={() => addItemToCart(item)}
+            />
+          ))
+        )}
       </div>
     </>
   );
